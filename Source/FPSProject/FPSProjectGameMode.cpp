65fea687c846352c8cd8e9f375f9f216646abd4c
NoKnees <68980704+NoKnees@users.noreply.github.com>
2020-07-30 21:43:35 -0700

    Inital Build

---
+++ Source/FPSProject/FPSProjectGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FPSProjectGameMode.h"
+#include "FPSProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AFPSProjectGameMode::AFPSProjectGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
